9545fda10897f572cd4e031b1d7c541349cb00e7
wasi <wasi_shah@ml-bench.com>
2026-04-08 13:05:57 +0500

    nextjs version updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-  outputFileTracingRoot: "/Users/mac/Desktop/woven art",
 };
 
 export default nextConfig;
